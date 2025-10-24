886b42043db586874a7ba6a75536cd5d2449cf55
samar-sadaat <sammarsadaat@gmail.com>
2025-10-24 23:29:02 +0500

    setup mongoulr

--- utils/dataBase.js
+++ utils/dataBase.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
+
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.Mongooes_Url);
+        await mongoose.connect("mongodb://localhost:27017/HostelX");
         console.log("Connected to MongoDB");
     } catch (err) {
         console.error(" MongoDB connection error:", err);
